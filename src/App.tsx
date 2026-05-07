455af0f127d5decbeb6013771ab72a73a80bc7b2
Nicolas Inchaustegui <530nicolas@gmail.com>
2026-05-07 14:34:43 -0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
-import Home from './pages/Home/Home';
-import IntroAnimation from './components/common/IntroAnimation/IntroAnimation';
-
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-
   return (
-    <>
-      {showIntro && <IntroAnimation onComplete={() => setShowIntro(false)} />}
-      <Home />
-    </>
-  );
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
